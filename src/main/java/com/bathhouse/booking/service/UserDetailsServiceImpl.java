d61a60cc472a43c919ddf6cbd266d7de7a491917
Afiyev Bakdaulet <afiyev.bakdaulet@gmail.com>
2022-06-12 14:07:04 +0600

    add UserDetailsServiceImpl service

---
+++ src/main/java/com/bathhouse/booking/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bathhouse.booking.service;
+
+import com.bathhouse.booking.model.Role;
+import com.bathhouse.booking.model.User;
+import com.bathhouse.booking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null)
+            throw new UsernameNotFoundException("Could not find user");
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for(Role role : user.getRoles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return new org.springframework.security.core.userdetails.User
+                (user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+}
