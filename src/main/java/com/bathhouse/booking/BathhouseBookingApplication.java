75404fd15115403c4ba00cf51035ebdbb9b8f2b5
Afiyev Bakdaulet <afiyev.bakdaulet@gmail.com>
2022-06-12 12:16:40 +0600

    Initial commit

---
+++ src/main/java/com/bathhouse/booking/BathhouseBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bathhouse.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathhouseBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BathhouseBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bathhouse/booking/BathhouseBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bathhouse.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BathhouseBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
